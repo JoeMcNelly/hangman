c5b563be572ed1820158070921c857a1e77b07e1
unknown <mcnelljd@MCNELLJD-1.rose-hulman.edu>
2014-11-04 21:49:21 -0500

    started making a base file for the javascript

---
+++ hangman.js
@@ -0,0 +1,68 @@
+var gl;
+var canvas;
+var colors = [vec4(0.4,0.0,0.0,1.0),//red
+              vec4(0.0,0.4,0.0,1.0),//green
+              vec4(0,0,0,1),//black
+              vec4(1,1,1,1)//white
+            ]
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+  
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1, 1, 1, 1 ); //white for now
+	gl.enable(gl.DEPTH_TEST);
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    //add other gl setup things here for 3d stuffs and whatnot
+    
+    
+    //add button/key listeners
+    
+    render();
+
+}
+function render()
+{
+    //do normal render things
+
+    drawBackground();
+
+    if (score > 0){
+        drawHead();
+        if (score > 1){
+            drawTorso();
+            if(score > 2){
+                drawLeftArm();//etc until the game is over
+            }
+        }
+    }
+}
+
+function drawHead(){
+//push the points to the buffers and whatnot
+}
+function drawTorso(){
+//push the points to the buffers and whatnot
+}
+function drawLeftArm(){
+//push the points to the buffers and whatnot
+}
+function drawRightArm(){
+//push the points to the buffers and whatnot
+}
+function drawLeftLeg(){
+//push the points to the buffers and whatnot
+}
+function drawRightLeg(){
+//push the points to the buffers and whatnot
+}
